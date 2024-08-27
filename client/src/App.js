c9e57e2a7e672c646a228a41e9894e9e567e7062
dylandSteven <rebmxict@gmail.com>
2024-08-27 13:04:56 +0800

    AI - yahoo finance news analyzer

---
+++ client/src/App.js
@@ -0,0 +1,66 @@
+import './App.css';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { Grid } from '@mui/material';
+import { useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const API_SERVER = 'http://localhost:8000';
+  const [url, setUrl] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [rate, setRate] = useState('');
+  const [error, setError] = useState('');
+
+  return (
+    <div className="App">
+      <br />
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            id="article-url"
+            label="Article URL"
+            variant="outlined"
+            style={{width: '80%'}}
+            onChange={(e) => {setUrl(e.target.value)}}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            variant="outlined"
+            style={{width: '80%'}}
+            disabled={loading}
+            onClick={() => {
+              setLoading(true);
+              axios.post(`${API_SERVER}/rate`, { url })
+              .then(response => {
+                if (response.status == 200) {
+                  setRate(response.data.msg);
+                }
+                setLoading(false);
+              })
+              .catch(error => {
+                console.log(error);
+                setLoading(false);
+              });
+            }}
+          >
+            Rate Article
+          </Button>
+        </Grid>
+        {rate ? (
+          <Grid item xs={12} style={{display: 'flex', justifyContent: 'center'}}>
+            <h3 style={{width: '80%'}}>Rate: {rate}</h3>
+          </Grid>
+        ) : ''}
+        {error ? (
+          <Grid item xs={12} style={{display: 'flex', justifyContent: 'center'}}>
+            <h3 style={{color: 'red', width: '80%'}}>Error: {error}</h3>
+          </Grid>
+        ) : ''}
+      </Grid>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/app.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, make_response, jsonify, send_from_directory
+import requests
+import re
+import json
+from bs4 import BeautifulSoup
+from openai import OpenAI
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+GPT_API_KEY = os.getenv('GPT_API_KEY')
+CLIENT_PATH = os.getenv('CLIENT_PATH')
+client = OpenAI(api_key=GPT_API_KEY)
+
+app = Flask(__name__, static_folder=CLIENT_PATH, static_url_path='/')
+CORS(app)
+
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:path>')
+def catch_all(path):
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/test')
+def hello():
+    return make_response(jsonify({'msg': 'Hello World!'}), 200)
+
+@app.route('/rate', methods=['POST'])
+def upload_webs():
+    if request.method == 'POST':
+        data = request.json
+        url = data['url']
+        try:
+            print("=====================")
+            response = requests.get(url, timeout=60)
+            text = response.content.decode('utf-8')
+            with open('a.txt', 'w', encoding='utf-8') as f:
+                f.write(text)
+            body = text.split('<div class="caas-body">')[1]
+            print("1")
+            body = body.split('<div id="view-cmts-cta')[0]
+            body = re.sub(r'<div\b[^>]*>(.*?)</div>', '', body)
+            body = body.replace('</div>', '')
+            soup = BeautifulSoup(body, 'html.parser')
+            body = soup.get_text()
+            print(body)
+            print("=====================")
+            # return make_response(jsonify({'msg': '6'}), 200)
+            chat = client.chat.completions.create(
+                messages=[
+                    {
+                        "role": "user",
+                        "content": f'''
+                            Please act as a professional analyst for the American stock market and do the following:
+                            Summaries the article and based on the sentiment you understand from the news, how would you rank that news from 1 to 10. (1- is super bad, 10- is excellent)
+                            Finally, provide me only rank number value.\n
+                            `{body}`
+                        ''',
+                    }
+                ],
+                model="gpt-3.5-turbo",
+            )
+            chat = chat.model_dump_json()
+            chat = json.loads(chat)
+            print(chat)
+            return make_response(jsonify({'msg': chat['choices'][0]['message']['content']}), 200)
+        except Exception as e:
+            print(str(e))
+            return make_response(jsonify({'msg': str(e)}), 400)
+
+    return make_response(jsonify({'msg': 'Done'}), 200)
+
+if __name__== '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8000)
